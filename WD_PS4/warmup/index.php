149cc31d47de82a8101bc069e7d7578bd1553f81
alekszavialov <avelhar74@gmail.com>
2018-10-09 01:13:13 +0300

    add wd4

---
+++ WD_PS4/warmup/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+unclude_once("common.php") ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="" method="post">
+            <input type="submit" name="function" value="addNumber">
+        </form>
+    </div>
+</body>
+</html>
