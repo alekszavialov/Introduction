f3810d6e376db32bc3ca522d7aaf17526056b773
alekszavialov <avelhar74@gmail.com>
2018-10-17 08:51:25 +0200

    add all jquery code to document ready

--- WD_PS3/Landing_page/js/common.js
+++ WD_PS3/Landing_page/js/common.js
@@ -1,40 +1,43 @@
-$("#show-form").click(function() {
-	$("#phone-form").removeClass("hidden");
+$(function() { 
+	$("#show-form").click(function() {
+		$("#phone-form").removeClass("hidden");
+	});
+
+	$("#phone-form-close").click(function() {
+		$("#phone-form").addClass("hidden");
+	});
+
+	$(".up-page").click(function() {
+		$("html").animate({
+			scrollTop: 0
+		}, "slow");
+	});
+
+	$(document).scroll(function() {
+		const minScroll = 10;
+		const scroll = $(this).scrollTop() > minScroll;
+		const $upPage = $(".up-page");
+		if (scroll && $upPage.hasClass("hidden")) {
+			$upPage.removeClass("hidden");
+		} else if (!scroll && !$upPage.hasClass("hidden")) {
+			$upPage.addClass("hidden");
+		}
+	});
+
+	$('a[href^="#"]').click(function(event) {
+		event.preventDefault();
+		const $element = $($.attr(this, 'href'))
+		const elOffset = $element.offset().top;
+		const elHeight = $element.height();
+		const windowHeight = $(window).height();
+		const animationSpeed = 500;
+		let offset = elOffset;
+		if (elHeight < windowHeight && elOffset > windowHeight / 2) {
+			offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+		}
+		$('html').animate({
+			scrollTop: offset
+		}, animationSpeed);
+	});
 });
 
-$("#phone-form-close").click(function() {
-	$("#phone-form").addClass("hidden");
-});
-
-$(".up-page").click(function() {
-	$("html").animate({
-		scrollTop: 0
-	}, "slow");
-});
-
-$(document).scroll(function() {
-	const minScroll = 10;
-	const scroll = $(this).scrollTop() > minScroll;
-	const $upPage = $(".up-page");
-	if (scroll && $upPage.hasClass("hidden")) {
-		$upPage.removeClass("hidden");
-	} else if (!scroll && !$upPage.hasClass("hidden")) {
-		$upPage.addClass("hidden");
-	}
-});
-
-$('a[href^="#"]').click(function(event) {
-	event.preventDefault();
-	const $element = $($.attr(this, 'href'))
-	const elOffset = $element.offset().top;
-	const elHeight = $element.height();
-	const windowHeight = $(window).height();
-	const animationSpeed = 500;
-	let offset = elOffset;
-	if (elHeight < windowHeight && elOffset > windowHeight / 2) {
-		offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
-	}
-	$('html').animate({
-		scrollTop: offset
-	}, animationSpeed);
-});
--- WD_PS3/dropDownMenu/js/common.js
+++ WD_PS3/dropDownMenu/js/common.js
@@ -10,49 +10,53 @@ const selectItemsArray = [
 ["tile008.png", "Vikki O'Doherty"],
 ];
 
-const $itemsContainer = $(".select-items");	
-const $itemsHead = $(".select-head");
 const animationSpeed = "slow";
 
-(function () {
+$(function() { 
+
+	const $itemsContainer = $(".select-items");	
+	const $itemsHead = $(".select-head");
+
 	let container;
 	$.each(selectItemsArray, function(key,value) {
 		container = $("<li> /").addClass("select-item").appendTo($itemsContainer);
 		$("<img />", {src :  `img/${value[0]}`, alt : `${value[1]}`}).addClass("select-image").appendTo(container);
 		$("<span>").addClass("select-item-value").text(value[1]).appendTo(container);
 	});
-})();
 
-$itemsHead.click(function() {
-	toggleItems();
-});
-
-function toggleItems() {
-	if (!$($itemsContainer).is(':animated')) {
-		$(".select").toggleClass("select-active");
-		$itemsContainer.slideToggle(animationSpeed);
-	}
-}
-
-$itemsContainer.on("click", "li", function() {	
-	if (!$($itemsContainer).is(':animated')) {
-		const $selectedImg = $(this).find('img');
-		$(".selected").removeClass("selected");
-		$(this).addClass("selected");
-		$(".select-head-selected").text($(this).text());	
-		$(".select-head-image").attr({
-			src: $selectedImg.attr('src'),
-			alt: $selectedImg.attr('alt')
-		});
+	$itemsHead.click(function() {
 		toggleItems();
-	}
-});
+	});
 
-$("body").click(function(event) {
-	if(event.target.className !== "select" && $('.select').hasClass('select-active')){
-		toggleItems();
+	function toggleItems() {
+		if (!$($itemsContainer).is(':animated')) {
+			$(".select").toggleClass("select-active");
+			$itemsContainer.slideToggle(animationSpeed);
+		}
 	}
-});
+
+	$itemsContainer.on("click", "li", function() {	
+		if (!$($itemsContainer).is(':animated')) {
+			const $selectedImg = $(this).find('img');
+			$(".selected").removeClass("selected");
+			$(this).addClass("selected");
+			$(".select-head-selected").text($(this).text());	
+			$(".select-head-image").attr({
+				src: $selectedImg.attr('src'),
+				alt: $selectedImg.attr('alt')
+			});
+			toggleItems();
+		}
+	});
+
+	$("body").click(function(event) {
+		if(event.target.className !== "select" && $('.select').hasClass('select-active')){
+			toggleItems();
+		}
+	});
+});	
+
+
 
 
 
