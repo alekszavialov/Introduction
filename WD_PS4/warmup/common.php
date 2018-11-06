4ee72e1af4e83b4ba77c9e9df36d60e71e029cab
alekszavialov <avelhar74@gmail.com>
2018-11-06 16:15:19 +0200

    change method and variable check

--- WD_PS4/warmup/common.php
+++ WD_PS4/warmup/common.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 const ERROR = "Incorrect input data";
-if (isset($_POST['function'])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['function'])) {
     switch ($_POST['function']) {
         case 'addNumbers':
             $_SESSION["addNumbers"] = addNumbers(false);
