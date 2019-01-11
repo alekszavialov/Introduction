def75cdaccdd08fda2186d1fa1e92a02c6c4db63
alekszavialov <avelhar74@gmail.com>
2019-01-11 03:23:54 +0200

    remove trash

--- WD_PS6/public/index.php
+++ WD_PS6/public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-error_reporting(-1);
-
 session_start();
 
 use core\Router;
@@ -11,20 +9,7 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 define('CORE', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'core');
 define('ROOT', dirname(__DIR__));
 define('APP', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app');
-define('SALT', '7PqivZ-Ija39JaszabP_gR');
 define('LAYOUT', 'default');
-
-
-//$pass_hash = password_hash("aaaaaaaaaaaaaaaa", PASSWORD_BCRYPT);
-//
-//echo $pass_hash;
-//if (password_verify('aaaaaaaaaaaaaaaa', $pass_hash)) {
-//    echo 'Password is valid!';
-//} else {
-//    echo 'Invalid password.';
-//}
-//
-//die();
 
 require ROOT . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'autoloader.php';
 
