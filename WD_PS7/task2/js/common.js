d26c6cf65758499ed744255a04e9d71f972347fb
alekszavialov <avelhar74@gmail.com>
2019-01-15 18:03:22 +0200

    add task 2

--- WD_PS7/task2/js/common.js
+++ WD_PS7/task2/js/common.js
@@ -1,54 +1,28 @@
 $(function () {
 
     const $form = $("#mainForm");
+    const $input = $("#input");
+    const $text = $("#text");
 
-    const regEx = {
-        ip: /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])$/,
-        url: /^(?:https?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:\/?#[\]@!$&'()*+,;=.]+$/i,
-        email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
-        date: /^(?:0[0-9]|1[0-2])\/(?:[0-2][0-9]|3[0-1])\/[0-9]{4}$/,
-        time: /^(?:[0-1][1-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/
-    };
 
-    $form.on("change", "input", function () {
-        validationData($(this).attr("name"), $(this).val());
-    });
+
 
     $form.on("submit", (function (e) {
         e.preventDefault();
-        let errorCount = 0;
-        $("#" + $form.attr("id") + " input").each(function () {
-            if (false === validationData($(this).attr("name"), $(this).val())) {
-                errorCount++;
-            }
+        const text = $('<div/>').text($text.val()).html();
+        const inputRegex = /^\/(?<regex>.*)\/(?<flags>\w*)?$/;
+        const inputText = inputRegex.exec($input.val());
+
+        console.log(text);
+        console.log(inputText.groups.flags);
+        const regex = new RegExp(inputText.groups.regex, inputText.groups.flags);
+
+        let newString = text.replace(regex, function(p) {
+            return `<mark>${p}</mark>`;
         });
-        if (errorCount !== 0) {
-            return;
-        }
-        $.ajax({
-            type: "POST",
-            url: "../handler/handler.php",
-            data: {
-                data: $form.serializeArray()
-            },
-            dataType: 'json',
-            cache: false
-        }).done(function (response) {
-            $.each(response, function (name, value) {
-                validationData(name, value);
-            });
-        }).fail(function (xhr) {
-            console.log(xhr.responseText);
-        });
+        $('#test').empty().append(newString);
+
     }));
 
-    function validationData(name, value) {
-        const $answerField = $("#" + name + "Response");
-        if (regEx[name].test(value)) {
-            $answerField.text('true');
-            return true;
-        }
-        $answerField.text('false');
-        return false;
-    }
+
 });
